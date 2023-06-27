182e2f482faa8c779316005c42584720d4c9b7b8
Mutaal Khan <95968359+mutaal-khan@users.noreply.github.com>
2023-06-26 22:49:02 -0700

    New Changes and v2.2.0

---
+++ src/js/animation.js
@@ -0,0 +1,13 @@
+anime({
+    targets: '.lpt_screen .lpt_app .lpt_app_window .lpt_app_container',
+    translateY: -280,
+    direction: 'alternate',
+    loop: true,
+    easing: 'linear',
+    duration: 2500,
+    background: 'red', 
+    endDelay: 500,
+    delay: 500,
+});
+
+
